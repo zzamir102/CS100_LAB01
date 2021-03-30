f9e2b41926ca3a9552bb5badd6aa43a1b2ad9402
Zamir Noor <zamirnoor98@gmail.com>
2021-03-30 12:38:01 -0700

    Add one more rectangle and compute its area

--- src/main1.cpp
+++ src/main1.cpp
@@ -1,15 +1,19 @@
-#include <iostream>
+nclude <iostream>
 #include "../header/rectangle.hpp"
 
 using namespace std;
 
 int main()
 {
-	Rectangle rect;
-	rect.set_width(3);
-	rect.set_height(4);
-	
-	cout << "Rectangle area: " << rect.area() << endl;
+    Rectangle rect1, rect2;
+    rect1.set_width(3);
+    rect1.set_height(4);
 
-	return 0;
+    rect2.set_width(4);
+    rect2.set_height(2);
+
+    cout << "Rectangle 1 area: " << rect1.area() << endl;
+    cout << "Rectangle 2 area: " << rect2.area() << endl;
+
+    return 0;
 }
